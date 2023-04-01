9c3b11c671d3ccefac75bf8435ed866031866177
orozaliev0 <orozaliev.pp@gmail.com>
2023-04-01 21:07:02 +0600

    platform

---
+++ src/components/books/Books.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Books.scss"
+
+
+const Books = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Books;
---
+++ src/components/page/subscription/Subscription.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "Subscription.scss"
+
+
+const Subscription = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Subscription;
