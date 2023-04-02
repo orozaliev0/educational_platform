0d7eb882ef7eeda63a8986397602da8321371fb1
timurbakirdinov5 <115129249+timurbakirdinov5@users.noreply.github.com>
2023-04-02 23:32:34 +0600

    tatata (#3)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.scss';
+
+import Footer from "./components/footer/Footer";
+
 import Acardion from "./components/Acardion";
+
 
 function App() {
     return (
         <div className="App">
+
+            <Footer/>
+
            <Acardion/>
+
         </div>
     );
 }
