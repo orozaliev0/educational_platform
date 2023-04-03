47926dd3a2311178d3657a4afcaa43d00fa39fc8
orozaliev0 <orozaliev.pp@gmail.com>
2023-04-03 15:18:07 +0600

    more

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.scss';
+import {Routes, Route} from "react-router-dom";
+import MainPage from "./components/main-page/MainPage";
+import Subscription from "./components/page/subscription/Subscription";
 
 function App() {
     return (
         <div className="App">
-
+            <Routes>
+                <Route path={"/" } element={<MainPage/>}/>
+                <Route path={'subscription'} element={<Subscription/>}/>
+            </Routes>
         </div>
     );
 }
