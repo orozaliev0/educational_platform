00499fa3e982282212111a5a23e80d543ff7a6bb
orozaliev0 <orozaliev.pp@gmail.com>
2023-04-05 11:57:59 +0600

    neew

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import './App.scss';
 import {Routes, Route} from "react-router-dom";
 import MainPage from "./components/main-page/MainPage";
 import Subscription from "./components/page/subscription/Subscription";
-
 import Footer from "./components/footer/Footer";
-
-import Acardion from "./components/Acardion";
+import Expectation from "./components/page/expectation/Expectation";
+import Success from "./components/page/success/Success";
+import ToMore from "./components/page/to_more/ToMore";
 
 
 function App() {
@@ -13,10 +13,11 @@ function App() {
         <div className="App">
             <Routes>
                 <Route path={"/" } element={<MainPage/>}/>
-                <Route path={'subscription'} element={<Subscription/>}/>
+                <Route path={'/subscription'} element={<Subscription/>}/>
+                <Route path={'/expectation'} element={<Expectation/>}/>
+                <Route path={'/success'} element={<Success/>}/>
+                <Route path={'/to_more'} element={<ToMore/>}/>
             </Routes>
-
-           <Acardion/>
             <Footer/>
             </div>
     );
