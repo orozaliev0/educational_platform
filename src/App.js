1e0a0dbc2278d297f1ffe40fdaabee78eb710e54
timurbakirdinov5 <115129249+timurbakirdinov5@users.noreply.github.com>
2023-04-05 11:24:22 +0600

    Books (#13)
    * i finished
    
    * not finished books

--- src/App.js
+++ src/App.js
@@ -1,4 +1,24 @@
 import './App.scss';
+
+import Footer from "./components/footer/Footer";
+import Acardion from "./components/Acardion";
+import Books from "./components/books/Books";
+import {Route, Routes} from "react-router-dom";
+import NewBooks from "./components/books/pageBooks/newBooks";
+
+
+function App() {
+    return (
+        <div className="App">
+            <Books/>
+           <Acardion/>
+            <Footer/>
+
+            <Routes>
+                <Route path={'/newBooks'} element={ <NewBooks/>}/>
+            </Routes>
+        </div>
+
 import MainPage from "./components/main-page/MainPage";
 import Subscription from "./components/page/subscription/Subscription";
 import OurCourses from "./components/our courses/Our Courses";
@@ -18,6 +38,7 @@ function App() {
             </Routes>
 
         </>
+
     );
 }
 
