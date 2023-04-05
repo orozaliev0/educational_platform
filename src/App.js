4840c5e6c0ecfb30f669a3a4fd457ebd6fe19608
timurbakirdinov5 <timaulanbekov5@gmail.com>
2023-04-05 11:22:51 +0600

    not finished books

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import './App.scss';
-
 import Footer from "./components/footer/Footer";
-
 import Acardion from "./components/Acardion";
+import Books from "./components/books/Books";
+import {Route, Routes} from "react-router-dom";
+import NewBooks from "./components/books/pageBooks/newBooks";
 
 
 function App() {
     return (
         <div className="App">
+            <Books/>
            <Acardion/>
             <Footer/>
+
+            <Routes>
+                <Route path={'/newBooks'} element={ <NewBooks/>}/>
+            </Routes>
         </div>
     );
 }
